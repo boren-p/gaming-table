439b3e944a11c296b9b3cf7e8008bd9683062131
Borenp <borenkpardo@gmail.com>
2025-11-27 10:44:31 -0500

    altura de header corregida

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,30 +3,28 @@ import logo from '../assets/logo.png';
 
 const Navbar = () => {
   return (
-    <nav className="bg-deep-forest-green text-parchment-cream p-4 shadow-md w-full">
-      <div className="w-full px-8 flex justify-between items-center">
-        <div className="flex items-center">
-          <img src={logo} alt="Gaming Table Logo" className="h-40 w-auto" />
-        </div>
+    <header className="relative bg-deep-forest-green h-18 text-parchment-cream  shadow-md w-full">
+      <img src={logo} alt="Gaming Table Logo" className="h-70 w-auto absolute top-0 left-0 z-10" />
+      <div className="pl-70 pr-20 w-full h-full flex justify-between items-center ">
         <ul className="flex space-x-6">
           <li>
-            <a href="#" className="hover:text-rustic-gold transition-colors duration-300">Home</a>
+            <a href="#" className=" hover:text-rustic-gold transition-colors duration-300">Home</a>
           </li>
           <li>
-            <a href="#" className="hover:text-rustic-gold transition-colors duration-300">Games</a>
+            <a href="#" className=" hover:text-rustic-gold transition-colors duration-300">Games</a>
           </li>
           <li>
-            <a href="#" className="hover:text-rustic-gold transition-colors duration-300">About</a>
+            <a href="#" className="  hover:text-rustic-gold transition-colors duration-300">About</a>
           </li>
           <li>
-            <a href="#" className="hover:text-rustic-gold transition-colors duration-300">Contact</a>
+            <a href="#" className="  hover:text-rustic-gold transition-colors duration-300">Contact</a>
           </li>
         </ul>
-        <button className="bg-rich-mahogany-brown hover:bg-rustic-gold text-parchment-cream font-bold py-2 px-4 rounded transition-colors duration-300">
+        <button className=" bg-rich-mahogany-brown hover:bg-rustic-gold text-parchment-cream font-bold py-2 px-4 rounded transition-colors duration-300">
           Sign In
         </button>
       </div>
-    </nav>
+    </header>
   );
 };
 
