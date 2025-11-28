7c8b75e49d28142bcbcc65bb6b500ccd61125e5a
Borenp <borenkpardo@gmail.com>
2025-11-28 08:36:17 -0500

    carrito de compras (solo svg)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 
   return (
     <header className="bg-deep-forest-green h-15 text-parchment-cream  shadow-md w-full">
-      <img src={logo} alt="Gaming Table Logo" className="size-55 absolute top-0 left-[2.5%]" />
+      <img src={logo} alt="Gaming Table Logo" className="size-55 absolute top-0 left-[2.5%] z-1" />
       <div className="pl-70 pr-20 w-full h-full flex justify-between items-center ">
         <ul className="flex space-x-6">
           <li>
@@ -38,6 +38,14 @@ const Navbar = () => {
               Create Account
               </button>}
         </div>
+        <div className='flex gap-5 items-center justify-center'>
+        {localStorage.getItem("role") === "cliente" && 
+        <div className='cursor-pointer'>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+          </svg>
+
+          </div>}
         {localStorage.getItem("role") && <button 
                   onClick={()=>{
                     localStorage.clear();
@@ -45,6 +53,8 @@ const Navbar = () => {
                   className=" bg-rich-mahogany-brown hover:bg-rustic-gold text-parchment-cream font-bold py-2 px-4 rounded transition-colors duration-300 cursor-pointer">
               Sign Out
               </button>}
+        
+        </div>
 
       </div>
     </header>
